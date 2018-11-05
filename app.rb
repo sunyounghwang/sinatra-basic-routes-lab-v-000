11f27fb7bddcfe8060e2f3e97ff5c6e1e7ee0827
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-05 19:12:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Sun."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is A Sunday Kind of Love by Etta James."
+  end
+
+  get '/hometown' do
+    "My hometown is Albany."
+  end
 end
